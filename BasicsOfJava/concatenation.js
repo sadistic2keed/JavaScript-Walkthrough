7117c32b529f891a6800909d5747326aa33e06e4
had4manS202 <hadamanash2023@gmail.com>
2025-06-18 07:07:39 +0545

    Practice

--- BasicsOfJava/concatenation.js
+++ BasicsOfJava/concatenation.js
@@ -3,6 +3,6 @@ const age = 10;
 
 let multipletext = `My name is ${username} and I am ${age} years old.
 I like playing chess.
-I sleep alot.`
+I sleep alot.
 console.log(multipletext);
 console.log("My name is " + username + " " + "and I am " + age + " " + "years old." );
--- JavaScript Fundamentals/comparison.js
+++ JavaScript Fundamentals/comparison.js
@@ -1,1 +1,23 @@
-console.log("AppleSample")
+console.log(50>30); //prints true
+
+console.log(50<30); //prints false
+
+console.log(50==30);
+
+
+// FOR STRINGS
+
+
+console.log("apple" > "banana") //prints false
+
+console.log("APPLE" == "apple") //prints false
+
+console.log("apple" == "apple")
+
+console.log("appl" < "apple") //prints true
+
+
+
+
+
+
