8acdff047f1a276518a72e1dc177c23ecd7a1a80
had4manS202 <hadamanash2023@gmail.com>
2025-06-19 22:25:52 +0545

    Fundamentals

--- JavaScript Fundamentals/logical.js
+++ JavaScript Fundamentals/logical.js
@@ -3,3 +3,26 @@
 // NOT !
 // NULLISH COALESCING ??
 
+// const name = "manas"
+// const nickname = "sanoj";
+
+// const username = name || nickname;
+
+
+// console.log(Boolean("Manas")) //true
+
+// console.log(Boolean("")); //false
+// console.log(Boolean(null)); //false
+// console.log(Boolean(undefined)); //false
+
+const a = '';
+const b = 2;
+console.log(a || b);
+
+const name = "Manash";
+const nickname = "Sanoj";
+
+const username = name && nickname; //prints sanoj
+// const username = name || nickname; //prints Manash
+
+console.log(`Name - ${username}`);
